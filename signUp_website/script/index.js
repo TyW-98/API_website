7cb72847b26fea7cdeeb804a6a88b2c68f336c57
wey1998 <yitwey1998@hotmail.com>
2023-04-12 01:28:23 +0100

    Use body-parser to read user input

--- signUp_website/script/index.js
+++ signUp_website/script/index.js
@@ -5,8 +5,19 @@ const https = require('https');
 
 const app = express();
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,"../signup.html"));
+});
+
+app.post('/', (req, res) => {
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const email = req.body.email
+
+    console.log(firstName + " " + lastName + " " + email)
 });
 
 app.listen(3000, function () {
