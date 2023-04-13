b7086cc6ba6be15d99a2cbcd5581bab7ac544145
wey1998 <yitwey1998@hotmail.com>
2023-04-13 17:47:19 +0100

    Store new subscriber to mailchimp

--- signUp_website/script/index.js
+++ signUp_website/script/index.js
@@ -1,25 +1,53 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const https = require('https');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const https = require("https");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,"../signup.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../signup.html"));
 });
 
-app.post('/', (req, res) => {
-    const firstName = req.body.firstName
-    const lastName = req.body.lastName
-    const email = req.body.email
+app.post("/", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
 
-    console.log(firstName + " " + lastName + " " + email)
+  console.log(firstName + " " + lastName + " " + email);
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FIRSTNAME: firstName,
+          LASTNAME: lastName,
+        },
+      },
+    ],
+  };
+  var jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/1ab5ee1e27";
+  const options = {
+    method: "POST",
+    auth: "AuxUser1:a43121af439423dbc369f0fb2bfe51a8-us21",
+  };
+
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {});
+  });
+
+  request.write(jsonData);
+  request.end();
+
 });
 
 app.listen(3000, function () {
-    console.log("Running on port 3000");
+  console.log("Running on port 3000");
 });
